0857c87065232f2762e4d2a35b4fc3256f86e3fa
Amith Reddy <amith@amith.ca>
2021-01-16 21:33:48 -0500

    Added a simple jinja template for image repo

--- flaskserver.py
+++ flaskserver.py
@@ -1,9 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'Hello, World!'
-
-
+    return render_template('home.html')
